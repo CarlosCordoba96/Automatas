f4d90445595ed739e10c32fc6acd0daa9923df34
catpilo <catpilo@users.noreply.github.com>
2017-05-11 17:20:18 +0200

    Removed duplicated methods & merge headers

--- Automatas/src/Carlos/Transicion.java
+++ Automatas/src/Carlos/Transicion.java
@@ -10,15 +10,11 @@ public class Transicion {
 		this.transicion=transicion;
 		
 	}
-	public Estado getDesde() {
-		return desde;
-	}
+
 	public void setDesde(Estado desde) {
 		this.desde = desde;
 	}
-	public Estado getHasta() {
-		return hasta;
-	}
+
 	public void setHasta(Estado hasta) {
 		this.hasta = hasta;
 	}
@@ -28,11 +24,11 @@ public class Transicion {
 		return "t "+desde.getEstado()+ "->" + hasta.getEstado() + "(" + transicion + ")";
 
 	}
-<<<<<<< HEAD
+
 	public String hashName(){
 		return desde.getEstado()+hasta.getEstado()+transicion;
 	}
-=======
+
 	
 	public Estado getDesde() {
 		return desde;
@@ -42,5 +38,5 @@ public class Transicion {
 		return hasta;
 	}
 
->>>>>>> latex
+
 }
